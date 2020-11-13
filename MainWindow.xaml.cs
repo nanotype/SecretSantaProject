57ee2c5b0b9597bab208cea8ab18438733ba6956
Victor PREVE <victor.preve@soprasteria.com>
2020-11-13 21:50:39 +0100

    first implementation of the software

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SecretSantaProject
+{
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<Member> Members = new ObservableCollection<Member>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			MemberList.ItemsSource = Members;
+		}
+
+		private void BT_Add_Member_Click(object sender, RoutedEventArgs e)
+		{
+			string prenom = NewUser_FirstName.Text;
+			string nom = NewUser_LastName.Text;
+			if(prenom != "")
+			{
+				Members.Add(new Member(prenom, nom).MAJ());
+				NewUser_FirstName.Clear();
+				NewUser_LastName.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Erreur système couche 8 !!!");
+				NewUser_FirstName.BorderBrush = new SolidColorBrush(Colors.Red);
+			}
+		}
+
+		private void BT_RAZ_Members_Click(object sender, RoutedEventArgs e)
+		{
+			Members.Clear();
+		}
+
+		private void BT_Delete_Member_Click(object sender, RoutedEventArgs e)
+		{
+			if(MemberList.SelectedItem != null)
+			{
+				Members.Remove(MemberList.SelectedItem as Member);
+			}
+		}
+	}
+}
---
+++ Member.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSantaProject
+{
+	class Member : INotifyPropertyChanged
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string CompleteName 
+		{
+			get { return FirstName + " " + LastName; }
+		}
+		public Member Target { get; set; }
+
+		public Member(string firstName, string lastName, Member target = null)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Target = target;
+		}
+
+		public Member MAJ()
+		{
+			NotifyPropertyChanged("Member");
+			return this;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void NotifyPropertyChanged(string PropertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		}
+	}
+}
